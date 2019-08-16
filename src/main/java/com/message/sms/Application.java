d22cd29f044e4938b42e9f607537ae1d086ceacd
zhouyingtao <zhouyingtao@58qf.com>
2019-08-16 18:02:01 +0800

    检测链接合理性----完成

--- src/main/java/com/message/sms/Application.java
+++ src/main/java/com/message/sms/Application.java
@@ -1,46 +1,140 @@
 package com.message.sms;
 
+import cn.hutool.core.collection.CollectionUtil;
 import cn.hutool.core.util.URLUtil;
 import cn.hutool.poi.excel.ExcelReader;
 import cn.hutool.poi.excel.ExcelUtil;
+import cn.hutool.poi.excel.ExcelWriter;
+import com.message.sms.pojo.UrlEntity;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Slf4j
 @SpringBootApplication
 public class Application {
 
-	@Autowired
-	SendSms sendSms;
+    public static void main(String[] args) {
+        log.info("main --------------------- start");
+        SpringApplication.run(Application.class, args);
+        log.info("main --------------------- end");
+//		System.out.println("main --------------------- end----------");
+        System.exit(0);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-		log.info("main --------------------- end");
-		System.out.println("main --------------------- end----------");
-//		sendSms.haha();
-		System.exit(0);
-	}
+    @Bean
+    public ApplicationRunner runner() {
+        return args -> {
+            AtomicInteger successNum = new AtomicInteger(0);
+            AtomicInteger errNum = new AtomicInteger(0);
+            String bookFilePath = null;
+            String name = "";
+            try {
+                File file = new File("D:/data");
+                String[] fileNameList = file.list();
+                for (String fileName : fileNameList) {
+                    if (fileName.endsWith(".xlsx")) {
+                        bookFilePath = "D:/data/" + fileName;
+                        name = fileName.substring(0, fileName.lastIndexOf("."));
+                        break;
+                    }
+                }
+            } catch (Exception e) {
+                log.error("file is not find==================", e);
+//				System.out.println("file is not find==================");
+                e.printStackTrace();
+            }
+            ExcelReader reader = ExcelUtil.getReader(bookFilePath);
+            List<Map<String, Object>> readAll = reader.readAll();
+            ArrayList<UrlEntity> resultUrlList = new ArrayList<>();
+            int sizeAll = readAll.size();
+            Boolean success = true;
+            for (int i = 0; i < sizeAll; i++) {
+                Map<String, Object> map = readAll.get(i);
+                String encodeUrl = "";
+                String url = "";
+                try {
+                    Object urlObject = map.get("链接");
+                    if (urlObject == null) {
+                        urlObject = map.get("URL");
+                    }
+                    if (urlObject == null) {
+                        success = false;
+                        break;
+                    }
+                    encodeUrl = urlObject.toString();
+                } catch (Exception e) {
+                    success = false;
+                    break;
+                }
+                try {
+                    url = URLUtil.decode(encodeUrl);
+                    if (url.contains("%")) {
+                        errNum.incrementAndGet();
+                        int id = i + 2;
+                        log.error(id + "\t" + encodeUrl + "\t" + url);
+                        resultUrlList.add(new UrlEntity(id,encodeUrl,url));
+                        continue;
+                    }
+                    String keywordPromotion = url.substring(url.lastIndexOf("=") + 1, url.length());
 
-	@Bean
-	public ApplicationRunner runner() {
-		return args -> {
-//			sendSms.haha();
-            ExcelReader reader = ExcelUtil.getReader("E:/data/新拓词.xlsx");
-            List<Map<String,Object>> readAll = reader.readAll();
-            readAll.forEach((Map<String, Object> map) -> {
-                String string = map.get("URL").toString();
-                String url = URLUtil.decode(string);
-                log.info(url);
-                System.out.println(url);
-            });
-		};
-	}
+                    String[] keywords = keywordPromotion.split("_");
+                    if (keywords.length < 6) {
+                        errNum.incrementAndGet();
+                        int id = i + 2;
+                        log.error(id + "\t" + encodeUrl + "\t" + url);
+                        resultUrlList.add(new UrlEntity(id,encodeUrl,url));
+                        continue;
+                    }
+                    successNum.incrementAndGet();
+                } catch (Exception e) {
+                    errNum.incrementAndGet();
+                    int id = i + 2;
+                    log.error(id + "\t" + encodeUrl + "\t" + url);
+                    resultUrlList.add(new UrlEntity(id,encodeUrl,url));
+                }
+            }
+            if (success) {
+                log.info("总数量Total:" + sizeAll + ",  成功successNum:" + successNum + ",  失败errNum:" + errNum);
+            } else {
+                log.error("没有发现--URL或者链接标题头------------------------");
+            }
+//			System.out.println("总数量Total:" + sizeAll + ", 成功successNum:" + successNum + ", 失败errNum:" + errNum);
+            if (CollectionUtil.isNotEmpty(resultUrlList)) {
+                log.info("生成Excel到桌面-------------------------开始");
+                writeToExcel(resultUrlList,name);
+            }
+        };
+
+    }
+
+    private void writeToExcel(ArrayList<UrlEntity> resultUrlList,String name) {
+        Map<String,String> map = System.getenv();
+//            System.out.println(map.get("USERNAME"));//获取用户名    C:\Users\EDZ\Desktop\elasticsearch.yml
+        // 通过工具类创建writer
+        String username = map.get("USERNAME");
+        String path = "C:/Users/"+username+"/Desktop/"+name+"--不规范URL-"+ new SimpleDateFormat("yyyyMMddHHmmss").format(new Date())+".xlsx";
+        ExcelWriter writer = ExcelUtil.getWriter(path);
+
+        writer.addHeaderAlias("id", "原excel中行号");
+        writer.addHeaderAlias("url_old", "原链接");
+        writer.addHeaderAlias("url", "解密后链接");
+        writer.merge(2, "不规范URL");
+        writer.write(resultUrlList, true);
+        writer.setColumnWidth(0,10);
+        writer.setColumnWidth(1,80);
+        writer.setColumnWidth(2,80);
+        writer.close();
+    }
 
 }
--- src/test/java/com/message/sms/personTest.java
+++ src/test/java/com/message/sms/personTest.java
@@ -2,9 +2,9 @@ package com.message.sms;
 
 import cn.hutool.poi.excel.ExcelReader;
 import cn.hutool.poi.excel.ExcelUtil;
-import com.message.sms.pojo.ExcelDEMO;
 
-import java.util.List;
+import java.io.File;
+import java.util.Map;
 
 /**
  * @Author: ZYT
@@ -16,11 +16,28 @@ public class personTest {
 
     public static void main(String[] args) throws Exception{
 
-        ExcelReader reader = ExcelUtil.getReader("E:/aaa.xlsx");
-//        读取为Bean列表，Bean中的字段名为标题，字段值为标题对应的单元格值。
+        File file = new File("E:/data");
+        String[] list = file.list();
+        String bookFilePath = null;
+        for (String fileName : list) {
+            if (fileName.endsWith(".xlsx")) {
+                bookFilePath = "E:/data/"+fileName;
+                System.out.println(bookFilePath);
+                break;
+            }
+        }
+        ExcelReader reader = ExcelUtil.getReader(bookFilePath);
+////        读取为Bean列表，Bean中的字段名为标题，字段值为标题对应的单元格值。
+//
+//        List<Person> excels = reader.readAll(Person.class);
+//        excels.forEach(x -> System.out.print(x));
 
-        List<ExcelDEMO> excels = reader.readAll(ExcelDEMO.class);
-        excels.forEach(x -> System.out.print(x));
+
+        Map<String,String> map = System.getenv();
+        System.out.println(map.get("USERNAME"));//获取用户名
+        System.out.println(map.get("COMPUTERNAME"));//获取计算机名
+        System.out.println(map.get("USERDOMAIN"));//获取计算机域名
+
 
     }
 }
